cd0f83ba3b2b8bd29ad7c8ac290d1908266cb1d4
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-19 17:37:10 +0200

    added fixes post code review

--- main.js
+++ main.js
@@ -3,18 +3,33 @@ var coffeeShop = {
   money: 100,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: {
+      beanRequirment: 10,
+      price: 3
+    },
+    americano: {
+      beanRequirment: 5,
+      price: 4
+    },
+    doubleShot: {
+      beanRequirment: 15,
+      price: 5
+    },
+    frenchPress: {
+      beanRequirment: 12,
+      price: 4
+    }
   },
 
   buySupplies: function (amountOfUnits) {
     var beansUnitPrice = 15;
     var total = amountOfUnits * beansUnitPrice;
-    if (this.money >= total) {
+
+    if (total <= this.money) {
+      this.beans += amountOfUnits;
+      console.log('there are ' + this.beans + ' units now in the shop');
       this.money = this.money - total;
-      console.log('there is ' + this.money + ' left');
+      console.log('there is $' + this.money + ' left');
     } else {
       alert("there is no more cash left in the shop");
     }
@@ -22,24 +37,32 @@ var coffeeShop = {
   },
 
   makeDrink: function (drinkType) {
-    if (this.drinkRequirements[drinkType] === undefined) {
+    if (!this.drinkRequirements[drinkType]) {
       alert("Sorry, we don't make " + drinkType);
     }
-    else if (this.beans < this.drinkRequirements[drinkType]) {
+    else if (this.beans < this.drinkRequirements[drinkType].beanRequirment) {
       alert("Sorry, we're all out of beans!");
     }
     else {
-      this.beans -= this.drinkRequirements[drinkType];
-      alert(this.beans);
+
+      alert("Enjoy your " + drinkType + " there are " + (this.beans -= this.drinkRequirements[drinkType].beanRequirment) + " beans left");
+
     }
+  },
+  buyDrink: function (drink) {
+    if (!this.drinkRequirements[drink]) {
+      alert('We dont sell that drink');
+      return;
+    } else
+      console.log("there is $" + (this.money += this.drinkRequirements[drink].price) + " in the register");
+    this.makeDrink(drink);
+
+
   }
 
 };
 
+coffeeShop.buyDrink('latte');
 
-//coffeeShop.buySupplies(1);
-//coffeeShop.makeDrink("americano");
-//coffeeShop.makeDrink("filtered");
-//coffeeShop.makeDrink("doubleShot");
-//coffeeShop.makeDrink("frenchPress");
+
 
