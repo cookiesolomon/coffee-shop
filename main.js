116569fe1410d58b1f963eda46b86ffdc7bab817
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-19 08:49:00 +0200

    first commit

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ var coffeeShop = {
 
   makeDrink: function (drinkType) {
     // TODO: Finish this method
+    if(drinkType !== drinkRequirements[drinkType]){
+      alert('Sorry, we dont make ' + drinkType);
+    } else if (drinkType === drinkRequirements[drinkType]){
+      this.beans
+    }
   }
 }
 
@@ -18,3 +23,9 @@ coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+
+
+
+/*Only allows you make a drink if it's a drink in the drinkRequirements object. Otherwise alert: "Sorry, we don't make DRINKNAME".
+If the drink is part of the drinkRequirements object, reduce the amount of beans by the number of beans required for that drink.
+If there are not enough beans to make that drink, alert: "Sorry, we're all out of beans!".*/
